1650c7fc599daa7672b7f5fb807c2a516a9b1555
Michael Koudela <xkoudel2@fi.muni.cz>
2023-04-23 18:06:44 +0200

    Update menu colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,52 @@
-import {AppBar, Box, Button, MenuItem, Toolbar, Typography} from "@mui/material";
-import {PageMenu} from "./PageMenu.tsx";
-import {useNavigate} from "react-router-dom";
+import {
+	AppBar,
+	Box,
+	Button,
+	MenuItem,
+	Toolbar,
+	Typography
+} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+import { PageMenu } from './PageMenu.tsx';
 
 export const Header = () => {
-    const navigate = useNavigate();
-    const pageItems = <>
-        <MenuItem key={'home'} onClick={() => navigate('/')}>
-            Home
-        </MenuItem>
-        <MenuItem key={'public'} onClick={() => navigate('/public-riddles')}>
-            Public riddles
-        </MenuItem>
-        <MenuItem key={'public'} onClick={() => navigate('/received-riddles')}>
-            Received riddles
-        </MenuItem>
-        <MenuItem key={'public'} onClick={() => navigate('/my-riddles')}>
-            My riddles
-        </MenuItem>
-    </>
+	const navigate = useNavigate();
+	const pageItems = (
+		<>
+			<MenuItem key="home" onClick={() => navigate('/')}>
+				Home
+			</MenuItem>
+			<MenuItem key="public" onClick={() => navigate('/public-riddles')}>
+				Public riddles
+			</MenuItem>
+			<MenuItem key="public" onClick={() => navigate('/received-riddles')}>
+				Received riddles
+			</MenuItem>
+			<MenuItem key="public" onClick={() => navigate('/my-riddles')}>
+				My riddles
+			</MenuItem>
+		</>
+	);
 
-    return(<AppBar sx={{ position: 'sticky', backgroundColor: "background.default"}}>
-        <Toolbar sx={{display: "flex", justifyContent: "space-between"}}>
-            <Typography color={"secondary"} variant={"h5"} fontWeight={"bold"}>
-                RiddleShare
-            </Typography>
-            <Box display={{xs: "block", md: "none"}}>
-                <PageMenu> {pageItems}</PageMenu>
-            </Box>
-            <Box display={{xs: "none", md: "flex"}} sx={{color: "secondary.light"}} flexDirection={"row"} gap={2}>
-                {pageItems}
-            </Box>
-        </Toolbar>
-    </AppBar>)
-}
+	return (
+		<AppBar sx={{ position: 'sticky', backgroundColor: 'background.default' }}>
+			<Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+				<Typography color="secondary" variant="h5" fontWeight="bold">
+					RiddleShare
+				</Typography>
+				<Box display={{ xs: 'block', md: 'none' }}>
+					<PageMenu> {pageItems}</PageMenu>
+				</Box>
+				<Box
+					display={{ xs: 'none', md: 'flex' }}
+					sx={{ color: 'secondary.light' }}
+					flexDirection="row"
+					gap={2}
+				>
+					{pageItems}
+				</Box>
+			</Toolbar>
+		</AppBar>
+	);
+};

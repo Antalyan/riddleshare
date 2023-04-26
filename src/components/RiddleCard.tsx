12f7021bb711dfa2f55960e2eef06e7d39636fa3
Michael Koudela <xkoudel2@fi.muni.cz>
2023-04-26 18:18:58 +0200

    Improve basic version of riddle card

--- src/components/RiddleCard.tsx
+++ src/components/RiddleCard.tsx
@@ -1,36 +1,75 @@
-import { Card, CardContent, CardHeader, CardMedia } from '@mui/material';
+import {
+	Card,
+	CardActions,
+	CardContent,
+	CardHeader,
+	CardMedia,
+	Chip
+} from '@mui/material';
 import {
 	CancelRounded,
 	CheckCircleOutlineRounded,
 	HelpOutlineRounded
 } from '@mui/icons-material';
+import { CircleFlag } from 'react-circle-flags';
+import LensIcon from '@mui/icons-material/Lens';
+import { useMemo } from 'react';
 
-type State = 'unsolved' | 'solved' | 'notstarted';
+import { CountryCode } from '../utils/CountryCodes.ts';
+import {
+	Difficulty,
+	DifficultyType,
+	getDifficultyObject
+} from '../utils/Difficulty.ts';
+import { RiddleStatus } from '../utils/Enums.ts';
+
 type Props = {
 	name: string;
 	image: string;
-	state: State;
+	state: RiddleStatus;
+	countryCode: CountryCode;
+	difficulty: Difficulty;
 };
 
-const getIcon = (state: State) => {
+const getIcon = (state: RiddleStatus) => {
 	switch (state) {
-		case 'notstarted':
+		case RiddleStatus.Unfinished:
 			return <HelpOutlineRounded />;
-		case 'solved':
+		case RiddleStatus.Solved:
 			return <CheckCircleOutlineRounded />;
 		default:
 			return <CancelRounded />;
 	}
 };
 
-export const RiddleCard = ({ name, image, state }: Props) => {
-	const x = '';
+export const RiddleCard = ({
+	name,
+	image,
+	state,
+	countryCode,
+	difficulty
+}: Props) => (
+	// const difficulty = useMemo(
+	// 	() => getDifficultyObject(difficultyType),
+	// 	[difficultyType]
+	// ); //TODO: Move to data fetching parent
 
-	return (
-		<Card>
-			<CardHeader action={getIcon(state)} />
-			<CardMedia component="img" image={image} alt={name} />
-			<CardContent>{name}</CardContent>
-		</Card>
-	);
-};
+	<Card>
+		<CardHeader action={getIcon(state)} />
+		<CardMedia component="img" image={image} alt={name} />
+		{/*//TODO: image fetching from db*/}
+		<CardContent>
+			{name}
+			<Chip
+				icon={<CircleFlag countryCode={countryCode} height={35} />}
+				label="Country name" //TODO: Replace with localized country name
+				variant="outlined"
+			/>
+			<Chip
+				icon={<LensIcon sx={{ color: difficulty?.color }} />}
+				label={difficulty?.name} //TODO: Replace with localized difficulty name
+				variant="outlined"
+			/>
+		</CardContent>
+	</Card>
+);

e1b20cbf6f373b0a95098facd4e0ea174e2fd63c
Michael Koudela <xkoudel2@fi.muni.cz>
2023-04-26 18:18:27 +0200

    Add difficulty and riddle state

---
+++ src/utils/Difficulty.ts
@@ -0,0 +1,20 @@
+export type Difficulty = {
+	name: string;
+	value: DifficultyType;
+	color: string;
+};
+
+export const Difficulties = [
+	{ name: 'very easy', value: 1, color: '#4caf50' },
+	{ name: 'easy', value: 2, color: '#8bc34a' },
+	{ name: 'medium', value: 3, color: '#ffff00' },
+	{ name: 'hard', value: 4, color: '#ff9100' },
+	{ name: 'extremely hard', value: 5, color: '#ff5722' }
+] as const;
+
+export type DifficultyType = (typeof Difficulties)[number]['value'];
+
+export const getDifficultyObject = (
+	difficultyValue: DifficultyType
+): Difficulty | undefined =>
+	Difficulties.find(diff => diff.value === difficultyValue) ?? undefined;
---
+++ src/utils/Enums.ts
@@ -0,0 +1,5 @@
+export enum RiddleStatus {
+	'Untouched',
+	'Unfinished',
+	'Solved'
+}

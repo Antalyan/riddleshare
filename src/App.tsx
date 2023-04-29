11ca548954b0ba4908c7283a8f499ff870d61e00
Michael Koudela <xkoudel2@fi.muni.cz>
2023-04-29 11:21:16 +0200

    Fix CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -30,35 +30,31 @@ export const App = () => {
 
 	return (
 		<ThemeProvider theme={theme}>
-			<CssBaseline>
-				<BrowserRouter>
-					<Header />
-					<Container
-						maxWidth="md"
-						component="main"
-						sx={{
-							alignItems: 'center',
-							display: 'flex',
-							flexDirection: 'column',
-							flexGrow: 1,
-							gap: 2,
-							justifyContent: 'center'
-						}}
-					>
-						{x}
-						<Routes>
-							<Route path="/" element={<HomePage />} />
-							<Route path="/my-riddles" element={<MyRiddlesPage />} />
-							<Route path="/public-riddles" element={<PublicRiddlesPage />} />
-							<Route
-								path="/received-riddles"
-								element={<ReceivedRiddlesPage />}
-							/>
-							<Route path="*" element={<NotFound />} />
-						</Routes>
-					</Container>
-				</BrowserRouter>
-			</CssBaseline>
+			<CssBaseline />
+			<BrowserRouter>
+				<Header />
+				<Container
+					maxWidth="md"
+					component="main"
+					sx={{
+						alignItems: 'center',
+						display: 'flex',
+						flexDirection: 'column',
+						flexGrow: 1,
+						gap: 2,
+						justifyContent: 'center'
+					}}
+				>
+					{x}
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/my-riddles" element={<MyRiddlesPage />} />
+						<Route path="/public-riddles" element={<PublicRiddlesPage />} />
+						<Route path="/received-riddles" element={<ReceivedRiddlesPage />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</Container>
+			</BrowserRouter>
 		</ThemeProvider>
 	);
 };

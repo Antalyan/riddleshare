3c29c97699657af93c7282fc69de8b441d031d83
Michael Koudela <xkoudel2@fi.muni.cz>
2023-04-29 18:45:34 +0200

    Tweak difficulty colors because of bad visibility

--- src/utils/Difficulty.ts
+++ src/utils/Difficulty.ts
@@ -8,8 +8,8 @@ export const Difficulties = [
 	{ name: 'trivial', value: 1, color: '#4dd0e1' },
 	{ name: 'easy', value: 2, color: '#4caf50' },
 	{ name: 'moderate', value: 3, color: '#ffff00' },
-	{ name: 'hard', value: 4, color: '#ff5722' },
-	{ name: 'extreme', value: 5, color: '#212121' }
+	{ name: 'hard', value: 4, color: '#ffab00' },
+	{ name: 'extreme', value: 5, color: '#bf360c' }
 ] as const;
 
 export type DifficultyType = (typeof Difficulties)[number]['value'];

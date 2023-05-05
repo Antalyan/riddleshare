59e07da4e8eb9d1d17229d581467174f2b2707f6
Michael Koudela <xkoudel2@fi.muni.cz>
2023-05-05 14:13:52 +0200

    Fixed broken deletion of questions

--- src/components/QuestionUpsertAccordion.tsx
+++ src/components/QuestionUpsertAccordion.tsx
@@ -26,53 +26,29 @@ type Props = {
 	control: Control<RiddleUpsertDetail>;
 };
 
-export const QuestionUpsertAccordion = ({ index, control }: Props) => {
-	const minLength = 1;
-
-	const { fields, remove } = useFieldArray({
-		name: `questions`,
-		control
-	});
-
-	const [disableDelete, setDisableDelete] = useState(true);
-	useEffect(() => {
-		setDisableDelete(fields.length === 1);
-		console.log(disableDelete);
-	}, [fields.length]);
-
-	return (
-		<Stack direction="row" alignItems="flex-start" width="100%">
-			<Accordion sx={{ flexGrow: 1 }}>
-				<AccordionSummary expandIcon={<ExpandMoreIcon />}>
-					<Typography variant="h6">Question {index + 1}</Typography>
-				</AccordionSummary>
-				<AccordionDetails>
-					<Stack gap={2} sx={{ minWidth: { md: 400 } }}>
-						<TextFieldElement
-							label="Question text"
-							multiline
-							name={`questions.${index}.questionText`}
-							rows={5}
-							// required
-							placeholder="Text of the question"
-						/>
-						<FileUploader
-							name={`questions.${index}.questionImage`}
-							control={control}
-							label="Question picture"
-						/>
-						<HintsUpsert control={control} questionIndex={index} />
-						<AnswersUpsert control={control} questionIndex={index} />
-					</Stack>
-				</AccordionDetails>
-			</Accordion>
-			{/*//TODO: Fix broken delete of question (works for subcomponents)*/}
-			<IconButton
-				onClick={() => fields.length > minLength && remove(index)}
-				sx={{ m: 1 }}
-			>
-				<Cancel />
-			</IconButton>
-		</Stack>
-	);
-};
+export const QuestionUpsertAccordion = ({ index, control }: Props) => (
+	<Accordion sx={{ flexGrow: 1 }}>
+		<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+			<Typography variant="h6">Question {index + 1}</Typography>
+		</AccordionSummary>
+		<AccordionDetails>
+			<Stack gap={2} sx={{ minWidth: { md: 400 } }}>
+				<TextFieldElement
+					label="Question text"
+					multiline
+					name={`questions.${index}.questionText`}
+					rows={5}
+					// required
+					placeholder="Text of the question"
+				/>
+				<FileUploader
+					name={`questions.${index}.questionImage`}
+					control={control}
+					label="Question picture"
+				/>
+				<HintsUpsert control={control} questionIndex={index} />
+				<AnswersUpsert control={control} questionIndex={index} />
+			</Stack>
+		</AccordionDetails>
+	</Accordion>
+);

3fc3b235c57fc202263ca1a1ae76d0be986faa14
Ondřej Dacer <dacer.ondrej@gmail.com>
2023-05-06 22:28:16 +0200

    Persist user

--- src/hooks/useLoggedInUser.tsx
+++ src/hooks/useLoggedInUser.tsx
@@ -8,11 +8,20 @@ const UserContext = createContext<User | undefined>(undefined);
 
 export const UserProvider: FC<PropsWithChildren> = ({ children }) => {
 	// Hold user info in state
-	const [user, setUser] = useState<User>();
+	const [user, setUser] = useState<User>(
+		sessionStorage.getItem('user')
+			? JSON.parse(sessionStorage.getItem('user'))
+			: undefined
+	);
 
 	// Setup onAuthChanged once when component is mounted
 	useEffect(() => {
-		onAuthChanged(u => setUser(u ?? undefined));
+		onAuthChanged(u => {
+			setUser(u ?? undefined);
+			if (u) {
+				sessionStorage.setItem('user', JSON.stringify(u));
+			}
+		});
 	}, []);
 
 	return <UserContext.Provider value={user}>{children}</UserContext.Provider>;

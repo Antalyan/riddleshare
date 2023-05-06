2464e16a2429b3d110e4b72974a26b3d727713a0
Ondřej Dacer <dacer.ondrej@gmail.com>
2023-05-06 22:51:16 +0200

    Implement `NotFound` page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,25 @@
 import type { FC } from 'react';
+import { Button, Stack, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-export const NotFound: FC = () => <div>Not Found</div>;
+export const NotFound: FC = () => {
+	const navigate = useNavigate();
 
-//TODO: Style better
+	return (
+		<Stack gap={2} alignItems="center" justifyContent="center">
+			<Typography variant="h2" textAlign="center">
+				Oops!
+			</Typography>
+			<Typography variant="h4" textAlign="center">
+				Page not found
+			</Typography>
+			<Typography textAlign="center">
+				Sorry, but the requested page is not found. Please go back to the
+				homepage.
+			</Typography>
+			<Button variant="contained" sx={{ mt: 2 }} onClick={() => navigate('/')}>
+				Go to homepage
+			</Button>
+		</Stack>
+	);
+};

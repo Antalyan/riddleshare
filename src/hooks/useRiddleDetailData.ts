743af1a0c0fc140fa6722de935093a22738b2b53
Michael Koudela <xkoudel2@fi.muni.cz>
2023-05-08 21:27:51 +0200

    Move data fetching to custom hook

---
+++ src/hooks/useRiddleDetailData.ts
@@ -0,0 +1,93 @@
+import { useEffect, useState } from 'react';
+import { onSnapshot, query, where } from 'firebase/firestore';
+
+import type {
+	RiddleDisplayDetail,
+	RiddleDisplayDetailSimple
+} from '../utils/Types';
+import { getDifficultyObject } from '../utils/Difficulty';
+import { RiddleStatus } from '../utils/Statuses';
+import {
+	riddlesCollection,
+	userRiddleInfoCollection
+} from '../datastore/firebase';
+
+import useLoggedInUser from './useLoggedInUser';
+
+export const useRiddleDetailData = (
+	linkId: string
+): RiddleDisplayDetailSimple => {
+	const user = useLoggedInUser();
+
+	const [riddle, setRiddle] = useState<RiddleDisplayDetailSimple>({
+		name: '',
+		description: '',
+		language: 'uk',
+		difficulty: getDifficultyObject(1),
+		linkId,
+		solvedText: '',
+		sharingInformation: { visibility: 'public' },
+		numberOfQuestions: -1,
+		solvedQuestions: -1,
+		state: RiddleStatus.Untouched
+	});
+
+	const qRiddle = query(riddlesCollection, where('linkId', '==', linkId));
+	const qSolveInfo = query(
+		userRiddleInfoCollection,
+		where('riddleLinkId', '==', linkId),
+		where('userEmail', '==', user?.email)
+	);
+
+	useEffect(
+		() =>
+			onSnapshot(qRiddle, snapshot => {
+				const riddleDoc = snapshot.docs[0];
+				const {
+					name,
+					description,
+					image,
+					language,
+					difficultyValue,
+					numberOfQuestions
+				} = riddleDoc.data();
+				setRiddle(prevState => ({
+					...prevState,
+					name,
+					description,
+					image,
+					language,
+					difficulty: getDifficultyObject(difficultyValue),
+					numberOfQuestions
+				}));
+			}),
+		[]
+	);
+
+	useEffect(
+		() =>
+			onSnapshot(qSolveInfo, snapshot => {
+				if (snapshot.docs.length === 0) {
+					setRiddle(prevState => ({
+						...prevState,
+						state: RiddleStatus.Untouched,
+						solvedQuestions: 0
+					}));
+					return;
+				}
+				const infoDoc = snapshot.docs[0];
+				const { state, questions } = infoDoc.data();
+				const solvedQuestions = Object.entries(questions).filter(
+					([, value]) => value.solved
+				).length;
+				setRiddle(prevState => ({
+					...prevState,
+					state,
+					solvedQuestions
+				}));
+			}),
+		[]
+	);
+
+	return riddle;
+};
--- src/pages/RiddleDetailPage.tsx
+++ src/pages/RiddleDetailPage.tsx
@@ -1,83 +1,18 @@
 import type { FC } from 'react';
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { onSnapshot, query, where } from 'firebase/firestore';
 
-import type { RiddleDisplayDetailSimple } from '../utils/Types';
-import { riddlesCollection, userRiddleInfoCollection } from '../firebase';
-import { getDifficultyObject } from '../utils/Difficulty';
 import { RiddleDetail } from '../components/RiddleDetail';
-import useLoggedInUser from '../hooks/useLoggedInUser';
-import { RiddleStatus } from '../utils/Enums';
+import { useRiddleDetailData } from '../hooks/useRiddleDetailData';
 
 //TODO: Add similar page with is creator view = false => (Figma: My riddles - detail)
 const RiddleDetailPage: FC = () => {
 	const { id } = useParams();
-	const user = useLoggedInUser();
-
-	//TODO: replace the default values and display only after the data are fetched from both queries
-	const [riddle, setRiddle] = useState<RiddleDisplayDetailSimple>({
-		name: '',
-		description: '',
-		language: 'uk',
-		difficulty: getDifficultyObject(1),
-		linkId: id ?? '',
-		solvedText: '',
-		sharingInformation: { visibility: 'public' },
-		numberOfQuestions: -1,
-		solvedQuestions: 0,
-		state: RiddleStatus.Untouched
-	});
-
-	const qRiddle = query(riddlesCollection, where('linkId', '==', id));
-	const qSolveInfo = query(
-		userRiddleInfoCollection,
-		where('riddleLinkId', '==', id),
-		where('userEmail', '==', user?.email)
+	const riddle = useRiddleDetailData(id ?? '');
+	return riddle.name !== '' && riddle.solvedQuestions !== -1 ? ( //This condition prevents too early display
+		<RiddleDetail isCreatorView={false} riddleDetail={riddle} />
+	) : (
+		<></>
 	);
-
-	useEffect(
-		() =>
-			onSnapshot(qRiddle, snapshot => {
-				const riddleDoc = snapshot.docs[0];
-				const {
-					name,
-					description,
-					image,
-					language,
-					difficultyValue,
-					numberOfQuestions
-				} = riddleDoc.data();
-				setRiddle(prevState => ({
-					...prevState,
-					name,
-					description,
-					image,
-					language,
-					difficulty: getDifficultyObject(difficultyValue),
-					numberOfQuestions
-				}));
-			}),
-		[]
-	);
-
-	useEffect(
-		() =>
-			onSnapshot(qSolveInfo, snapshot => {
-				const infoDoc = snapshot.docs[0];
-				const { state, questions } = infoDoc.data();
-				const solvedQuestions = Object.entries(questions).filter(
-					([, value]) => value.solved
-				).length;
-				setRiddle(prevState => ({
-					...prevState,
-					state,
-					solvedQuestions
-				}));
-			}),
-		[]
-	);
-	return <RiddleDetail isCreatorView={false} riddleDetail={riddle} />;
 };
 
 export default RiddleDetailPage;

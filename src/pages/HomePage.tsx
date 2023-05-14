a2b58de95bf84c08da5d8a7c046408b5f1579b09
Michael Koudela <73558534+Antalyan@users.noreply.github.com>
2023-05-15 00:37:09 +0200

    54 Add project information (#55)
    * Add image on home page
    
    * Added information about project on homepage
    
    ---------
    
    Co-authored-by: Michael Koudela <xkoudel2@fi.muni.cz>

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,29 @@
 import type { FC } from 'react';
-import { Box, Button, Stack, Typography } from '@mui/material';
+import { Box, Button, Link, Stack, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 import useLoggedInUser from '../hooks/useLoggedInUser';
+import { InfoLine } from '../components/riddleDetail/InfoLine';
 
 export const HomePage: FC = () => {
 	const navigate = useNavigate();
 	const user = useLoggedInUser();
 
 	return (
-		<Stack gap={2} alignItems="center" justifyContent="center">
+		<Stack gap={2} alignItems="center" justifyContent="flex-start">
+			<Box
+				component="img"
+				src="/public/RiddleMeThis.jpeg"
+				sx={{
+					maxWidth: '100%',
+					objectFit: 'contain',
+					objectPosition: 'center'
+				}}
+			/>
 			<Typography variant="h3" textAlign="center">
 				Create riddles, share them with your friends and have fun.
 			</Typography>
-			<Box sx={{ display: 'flex', gap: 1, mt: 2 }}>
+			<Box sx={{ display: 'flex', gap: 2, mt: 2 }}>
 				{user && (
 					<Button
 						variant="contained"
@@ -32,6 +42,23 @@ export const HomePage: FC = () => {
 					Try to solve a riddle
 				</Button>
 			</Box>
+			<Typography variant="h4" textAlign="center" sx={{ mt: 10 }}>
+				Project information
+			</Typography>
+			<Link href="https://github.com/Antalyan/riddleshare">
+				<Typography variant="h6">Github repository</Typography>
+			</Link>
+			<InfoLine label="Authors" value="Ondřej Dacer, Michael Koudela" />
+			<Typography variant="h6" textAlign="center">
+				Made as a final project in{' '}
+				<Typography variant="h6" component="span" fontWeight="bold">
+					PV247 Modern development of user interfaces
+				</Typography>{' '}
+				taught at{' '}
+				<Typography variant="h6" component="span" fontWeight="bold">
+					Faculty of Informatics, Masaryk University, Brno
+				</Typography>
+			</Typography>
 		</Stack>
 	);
 };

9a21c349931929bb798e9f454c81b9e31184b774
Michael Koudela <73558534+Antalyan@users.noreply.github.com>
2023-05-16 00:02:22 +0200

    Update RiddleDetail.tsx
    Remove duplicate import

--- src/components/RiddleDetail.tsx
+++ src/components/RiddleDetail.tsx
@@ -14,7 +14,6 @@ import {
 import useLoggedInUser from '../hooks/useLoggedInUser';
 import { useRiddleSolversDataFetch } from '../hooks/useRiddleSolversDataFetch';
 import { getDifficultyObject } from '../utils/Difficulty';
-import { getLanguage } from '../utils/CountryCodes';
 
 import { InfoLine } from './riddleDetail/InfoLine';
 import { InfoAccordion } from './riddleDetail/InfoAccordion';
